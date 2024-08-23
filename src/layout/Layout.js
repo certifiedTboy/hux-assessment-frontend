562b07296caf80b859e64f69fe8ffd9774264b35
Adebisi Tosin <79862233+certifiedTboy@users.noreply.github.com>
2024-08-23 02:13:53 +0100

    Update Layout.js
    Add seo

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -7,6 +7,37 @@ import Footer from "./Footer";
 const Layout = () => {
   const location = useLocation();
   const { pathname } = location;
+
+  let titleData;
+
+  if (pathname === "/home" || pathname === "/") {
+    titleData = {
+      title: "WebDev - Home",
+      metaDescription: "Home page of Wed-dev blog",
+    };
+  }   else if (pathname === "/contacts") {
+    titleData = {
+      title: "Hux - Contacts",
+      metaDescription: "all available contacts",
+    };
+  } else if (pathname === "/get-started/signin") {
+    titleData = {
+      title: "Hux - Get Started",
+      metaDescription: "sign in on hux to get started",
+    };
+  } else if (pathname === "/get-started/signup") {
+    titleData = {
+      title: "Hux - Get Started",
+      metaDescription: "Sign up on hux to get started",
+    };
+  }   else {
+    titleData = {
+      title: "404 Error - Page not found",
+      metaDescription: "Page not found",
+    };
+  }
+
+  SEO(titleData);
   return (
     <>
       <header className="fixed-top">
