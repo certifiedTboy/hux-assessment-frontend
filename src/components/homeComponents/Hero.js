961b7a0dc162ce5101c0e3c2ff42086b4e19a7fe
certifiedTboy <etosin70@gmail.com>
2024-08-23 01:17:49 +0100

    update ap

--- src/components/homeComponents/Hero.js
+++ src/components/homeComponents/Hero.js
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-slate-950 dark:bg-gray-900 mb-52">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a
           href="#"
@@ -11,7 +11,7 @@ const Hero = () => {
             New
           </span>{" "}
           <span className="text-sm font-medium">
-            Flowbite is out! See what's new
+            Hux is out! See what's new
           </span>
           <svg
             className="ml-2 w-5 h-5"
@@ -30,8 +30,7 @@ const Hero = () => {
           We invest in the world’s potential
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
+          Keep your contacts clean and accurate
         </p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
